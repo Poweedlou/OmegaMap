2fe3756174beacd7a7ba7b4f6b567839bd3a6cb3
Alexander Vinogradov <alexvingra@mail.ru>
2020-02-13 18:55:10 +0300

    Func to get image using coords

--- image_coords.py
+++ image_coords.py
@@ -1,14 +1,13 @@
 from io import BytesIO
 import requests
-from PIL import Image
 
 
 def get_bytes_image(coords, spn):
-    geocoder_api_server = "http://static-maps.yandex.ru/1.x/"
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
     map_params = {
         "ll": ', '.join(map(str, coords)),
         "spn": spn,
         "l": "map"
     }
-    response = requests.get(geocoder_api_server, params=map_params)
+    response = requests.get(map_api_server, params=map_params)
     return BytesIO(response.content)
